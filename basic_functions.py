95bdc9b2a106b3413020941f8127bbb8dce50d16
hiyayakko0 <hiyayakko0@users.noreply.github.com>
2017-02-26 23:24:08 +0900

    Update basic_functions.py

--- basic_functions.py
+++ basic_functions.py
@@ -1,4 +1,8 @@
 import bpy
-import math
+import object_printed3d_utils
 
-def delete_all()
+f = open('report.txt', '
+
+#objのself_intersect_facesを書き出し
+obj = bpy.data.objects[0]
+len(object_print3d_utils.mesh_helpers.bmesh_check_self_intersect_object(obj))
